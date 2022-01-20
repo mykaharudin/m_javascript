2ca90f1a5094e31c82a66789ec97b516d512e160
emyekah <leadload6@gmail.com>
2022-01-20 13:50:32 +0700

    brach

---
+++ dasar/branching.js
@@ -0,0 +1,7 @@
+var totalBelanja = prompt("Total belanja?","tolong diisi" );
+
+        if(totalBelanja > 100000){
+            document.write("<h2>Selamat Anda dapat hadiah</h2>");
+        }
+
+        document.write("<p>Terimakasih sudah berbelanja di toko kami</p>");
