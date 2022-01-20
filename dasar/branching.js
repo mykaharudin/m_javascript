6936d06e805453959dd289ee3763950b23ce5466
emyekah <leadload6@gmail.com>
2022-01-20 20:52:37 +0700

    cabang

--- dasar/branching.js
+++ dasar/branching.js
@@ -1,7 +1,13 @@
-var totalBelanja = prompt("Total belanja?","tolong diisi" );
+var totalspending = prompt("total spending? ","");
+if(totalspending>200000){
+    document.write("<h2> congrulation to You</h2> ");
+}
+document.write("thanks you very much");
 
-        if(totalBelanja > 100000){
-            document.write("<h2>Selamat Anda dapat hadiah</h2>");
-        }
-
-        document.write("<p>Terimakasih sudah berbelanja di toko kami</p>");
+var pass = prompt("Login your password: ");
+if (pass == "coffee") {
+    document.write("<h2>Welcome mas bro@ </h2>");
+} else {
+    document.write("Correct, speel again");
+}
+document.write("Thanks you very much everyone");
